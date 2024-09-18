4ef2c1d5476eb495a80b9c31dd83d407156f0104
Easy Destini Manik <72480830+easyManik@users.noreply.github.com>
2024-09-18 09:52:35 +0700

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,18 +3,18 @@ const path = require("path");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
-    mode: "development",
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "dist"),
-        },
-        watchFiles: ["index.html", "src/**/*"],
-        open: true,
-        client: {
-            overlay: {
-                errors: true,
-                warnings: false,
-            },
-        },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
     },
+    watchFiles: ["index.html", "src/**/*"],
+    open: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
 });
