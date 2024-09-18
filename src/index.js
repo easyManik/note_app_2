32f65951f0525a55eeb3d0830b8d50f7029d7e7d
Easy Destini Manik <72480830+easyManik@users.noreply.github.com>
2024-09-18 09:51:29 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import "./components/app-root.js";
 import "./styles/style.css";
+import main from "./script/main.js";
+import "./components/app-root.js";
+import "./components/note-form.js";
+import "./components/note-item.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  main();
+});
---
+++ src/script/main.js
@@ -0,0 +1,5 @@
+import "../components/app-root";
+
+const main = () => {};
+
+export default main;
