ccbb22c7624d0dcd759cf0b22f4f73ea3b1d9a54
Easy Destini Manik <72480830+easyManik@users.noreply.github.com>
2024-09-18 09:52:23 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,30 +2,23 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.join(__dirname, "src/index.js"),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-    },
-    module: {
-        rules: [
-            /* style and css loader */
-            {
-                test: /\.css$/,
-                use: [{
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "index.html"),
-            filename: "index.html",
-        }),
+  entry: path.join(__dirname, "src/index.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "index.html"),
+      filename: "index.html",
+    }),
+  ],
 };
